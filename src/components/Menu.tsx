41bb6d61b58c9444b04c19ffd2d40f27956efa93
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-06-28 14:47:58 +0100

    svg animation

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Menu.css';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+
+const pathVariants = {
+  initial: {
+    pathLength: 0,
+  },
+  animate: {
+    pathLength: 1,
+    transition: {
+      duration: 1.5,
+    }
+  }
+}
 
 const Menu = () => {
   return (
     <div id='menu'>
       <Link to='/'>
-        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-          <polyline points="16 18 22 12 16 6"></polyline>
-          <polyline points="8 6 2 12 8 18"></polyline>
-        </svg>
+      <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+        <motion.path variants={pathVariants} initial='initial' animate='animate' d="M16 18 22 12 16 6"/>
+        <motion.path variants={pathVariants} initial='initial' animate='animate' d="M8 6 2 12 8 18"/>
+      </svg>
       </Link>
       <div>
         <span>Projects</span>
