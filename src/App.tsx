522196549455f95b53468e17f65d711ab53810fd
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-02-03 21:45:12 -0300

    [version-2.0] cleans project

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import { Switch, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import Menu from './components/Menu';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Project from './pages/Projects';
 
 const App = () => {
-  const location = useLocation();
   return (
     <>
-      <Menu/>
-      <AnimatePresence exitBeforeEnter>
-        <Switch location={location} key={location.key}>
-          <Route path='/' exact>
-            <Home />
-          </Route>
-          <Route path='/contact'>
-            <Contact />
-          </Route>
-          <Route path='/projects'>
-            <Project />
-          </Route>
-        </Switch>
-      </AnimatePresence>
+      <h1> Version 2.0 Coming Soon </h1>
     </>
   );
 }
