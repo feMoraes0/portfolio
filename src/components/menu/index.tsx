eef7091ef7e78088c359d674b5c1af7a6d68acb6
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-02-27 00:07:12 -0300

    Updates menu to adjust in mobile screen size

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { MenuContainer, TopItemMenu, BottomItemMenu, LeftItemMenu, RightItemMenu } from './style';
+import { MenuContainer, TopItemMenu, BottomItemMenu, LeftItemMenu, RightItemMenu, MenuIcon, ItemText } from './style';
+import Git from '../../assets/icons/git.svg';
+import Building from '../../assets/icons/building.svg';
+import Mail from '../../assets/icons/mail.svg';
+import Person from '../../assets/icons/person.svg';
 
 const Menu = () => {
   return (
     <MenuContainer>
-      <TopItemMenu to="/">About</TopItemMenu>
-      <BottomItemMenu to="/contact">Contact</BottomItemMenu>
-      <LeftItemMenu to="/projects">Projects</LeftItemMenu>
-      <RightItemMenu to="/experiences">Experiences</RightItemMenu>
+      <TopItemMenu to="/">
+        <ItemText>About</ItemText>
+        <MenuIcon src={Person} alt="" />
+      </TopItemMenu>
+      <RightItemMenu to="/experiences">
+        <ItemText>Experiences</ItemText>
+        <MenuIcon src={Building} alt="" />
+      </RightItemMenu>
+      <LeftItemMenu to="/projects">
+        <MenuIcon src={Git} alt="" />
+        <ItemText>Projects</ItemText>
+      </LeftItemMenu>
+      <BottomItemMenu to="/contact">
+        <ItemText>Contact</ItemText>
+        <MenuIcon src={Mail} alt="" />
+      </BottomItemMenu>
     </MenuContainer>
   )
 };
--- src/components/menu/style.ts
+++ src/components/menu/style.ts
@@ -1,53 +1,111 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+
 const MenuContainer = styled.div`
   width: 100vw;
   position: fixed;
   z-index: 2;
 
-  @media (max-width: 375px) {
-    display: none;
+  @media only screen and (max-width: 375px) {
+    display: absolute;
+    bottom: 0;
+    left: 0;
+    height: 55px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #292830;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    border-top: .5px solid rgba(255, 255, 255, 0.1);
   }
 `;
 
-const ItemText = styled(Link)`
-  font-size: 1.80rem;
-  letter-spacing: 3px;
-  font-weight: 500;
+const Item = styled(Link)`  
   position: absolute;
   text-decoration: none;
   padding: 2vh 0px;
   text-align: center;
-  color: #F7F7F7;
-  font-family: 'Lexend Mega', sans-serif;
+
+  @media only screen and (max-width: 375px) {
+    position: relative;
+    padding: 0;
+    color: transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
-const TopItemMenu = styled(ItemText)`
+const ItemText = styled.span`
+  font-size: 1.80rem;
+  letter-spacing: 3px;
+  font-weight: 500;
+  color: #F7F7F7;
+  font-family: 'Lexend Mega', sans-serif;
+
+  @media only screen and (max-width: 375px) {
+    display: none;
+  }
+`;
+
+const TopItemMenu = styled(Item)`
   width: 100vw;
   top: 0;
   left: 0;
+
+  @media only screen and (max-width: 375px) {
+    width: 25%;
+    height: 100%;
+    border-top-left-radius: 10px;
+  }
 `;
 
-const BottomItemMenu = styled(ItemText)`
+const BottomItemMenu = styled(Item)`
   width: 100vw;
   top: 92vh;
   left: 0;
+
+  @media only screen and (max-width: 375px) {
+    width: 25%;
+    height: 100%;
+    top: 0;
+  }
 `;
 
-const LeftItemMenu = styled(ItemText)`
+const LeftItemMenu = styled(Item)`
   width: 100vh;
   top: 0;
   left: 0;
   transform: translate(-46vh, 46vh) rotate(90deg);
+
+  @media only screen and (max-width: 375px) {
+    width: 25%;
+    height: 100%;
+    transform: translate(0, 0);
+  }
 `;
 
-const RightItemMenu = styled(ItemText)`
+const RightItemMenu = styled(Item)`
   width: 100vh;
   top: 0;
   right: 0;
   transform: translate(46vh, 46vh) rotate(270deg);
+
+  @media only screen and (max-width: 375px) {
+    width: 25%;
+    height: 100%;
+    transform: translate(0, 0);
+    border-top-right-radius: 10px;
+  }
+`;
+
+const MenuIcon = styled.img`
+  color: white;
+  height: 30px;
 `;
 
 
-export { MenuContainer, TopItemMenu, BottomItemMenu, LeftItemMenu, RightItemMenu };
+export { MenuContainer, TopItemMenu, BottomItemMenu, LeftItemMenu, RightItemMenu, MenuIcon, ItemText };
